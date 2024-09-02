2cbcf8be1d5bf444c9a13e927eee32dd81a4b836
umaradil02 <158709535+umaradil02@users.noreply.github.com>
2024-09-03 01:22:26 +0500

    chek user detail

--- app2.js
+++ app2.js
@@ -15,6 +15,7 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
     console.log(user)
     // ...
   } else {
